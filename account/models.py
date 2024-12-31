ed38450cd5c903013c17fce25bb73ed442ab855a
nimazrz <Nimaaa8413@gmail.com>
2024-12-31 13:05:09 +0330

    bug fixed

--- account/models.py
+++ account/models.py
@@ -38,7 +38,7 @@ class CustomUserManager(BaseUserManager):
 
 class CustomUser(AbstractBaseUser, PermissionsMixin):
     # required
-    code_meli = models.CharField(unique=True, max_length=10, validators=[MinLengthValidator(10)])
+    code_meli = models.CharField(unique=True, max_length=10)
     username = models.CharField(unique=True, max_length=20)
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
@@ -75,7 +75,7 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
     
 
 class CommonFieldsMixin(models.Model): 
-    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='%(class)s')
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='%(class)s_user')
     
     class Meta: 
         abstract = True 
